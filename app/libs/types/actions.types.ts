fced1e4f33f70c45da19608db943ed8f268a2c2f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-04-29 14:42:39 -0600

    Add actions.types.ts

---
+++ app/libs/types/actions.types.ts
@@ -0,0 +1,16 @@
+import { requestQuantity } from "./API.types";
+
+export interface getLifeWildBase {
+    query: string;
+}
+
+export interface getLifeWildByVernacularNameProps extends getLifeWildBase {
+    quantity?: requestQuantity
+}
+
+export interface getLifeWildByBinomialNameProps extends getLifeWildBase {
+    quantity?: requestQuantity
+}
+
+export interface getLifeWildByIdProps extends getLifeWildBase {
+}
