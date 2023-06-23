3e8108120e2bd2ba6c3ccd69c6c86e8f28714c1e
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-23 17:18:19 -0400

    Fix broken outline

--- app/(components)/SectionBox.tsx
+++ app/(components)/SectionBox.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const SectionBox = ({ imagePath }: { imagePath: string }) => {
 
     return (
-        <div className="flex items-center justify-center outline outline-offset-0 outline-[6px] outline-lwgreen w-[2rem] h-[2rem] bg-inherit border-lwgreen rounded-md">
+        <div className="flex items-center justify-center border-4 outline-[6px] w-[2rem] h-[2rem] bg-inherit border-lwgreen rounded-md">
             <Image className="w-[1.2rem] h-full"
                 width={200} height={200} src={imagePath} alt="" />
         </div>
