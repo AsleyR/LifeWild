470f48953ad594f42768498070b879bc8c6bbad4
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-06-23 17:19:26 -0400

    Removed card description, make title & body small

--- app/(components)/resultcards/ResultCard.tsx
+++ app/(components)/resultcards/ResultCard.tsx
@@ -1,7 +1,6 @@
 import { ResultCardType } from "@/app/libs/types/ResultCard.type";
 import Link from "next/link";
 import ResultImage from "./ResultImage";
-import ResultCardDescription from "./ResultCardDescription";
 
 export default function ResultCard({ data }: { data: ResultCardType }) {
 
@@ -10,12 +9,11 @@ export default function ResultCard({ data }: { data: ResultCardType }) {
             className={`
         grid grid-cols-[auto_auto] gap-2 md:gap-12 bg-white drop-shadow rounded-md 
         cursor-pointer hover:scale-105 active:scale-95 transition-all
-        overlow-hidden h-[176px] min-w-0
+        oveflow-hidden h-[176px] min-w-0 items-center
         `}>
             <div className="pl-6 py-6">
-                <h1 className="font-bold text-2xl">{data.title}</h1>
-                <h2 className="italic text-sm">{data.scientificName}</h2>
-                <ResultCardDescription description={data.description[0]} />
+                <h1 className="font-bold text-lg md:text-2xl">{data.title}</h1>
+                <h2 className="italic text-base md:text-sm">{data.scientificName}</h2>
             </div>
             <div className="flex h-[176px] justify-end w-full">
                 <ResultImage image={data.imagePath} />
